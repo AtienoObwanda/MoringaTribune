83e4964cc7e7fa42286add987252d83c0783f3b2
atienoobwanda <atienoobwanda@gmail.com>
2022-05-23 20:51:55 +0300

    adding tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from . models import Editor, Article, tags
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.atieno=Editor(firstName='Atieno', lastName='Obwanda',email='atienoobwanda@gmail.com',phone_number='0743068355')
