16bee87cfb8708a24c742ff6c0531269dac901d9
atienoobwanda <atienoobwanda@gmail.com>
2022-05-23 20:17:34 +0300

    adding editor model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Editor(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    email = models.EmailField()
