a7df86084a09ee560ed3c85458688e5f56217904
atienoobwanda <atienoobwanda@gmail.com>
2022-05-23 21:35:31 +0300

    adding test class for articles

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from . models import Editor, Article, tags
 
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal= ('tags',)
+
 # Register your models here.
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(tags)
