20bcc45c907e3ab15d011d6db9e632c21c735296
atienoobwanda <atienoobwanda@gmail.com>
2022-05-23 20:35:01 +0300

    adding the model for tags

--- news/models.py
+++ news/models.py
@@ -6,3 +6,15 @@ class Editor(models.Model):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.firstName
+
+    class Meta:
+        ordering = ['firstName']
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+        
