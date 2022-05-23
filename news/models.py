bea4d9d832949f733349a9a546d61e1a70c65160
atienoobwanda <atienoobwanda@gmail.com>
2022-05-23 20:48:15 +0300

    adding tags model

--- news/models.py
+++ news/models.py
@@ -6,6 +6,8 @@ class Editor(models.Model):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
 
     def __str__(self):
         return self.firstName
@@ -15,6 +17,15 @@ class Editor(models.Model):
 
 class tags(models.Model):
     name = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
-        
+
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateField(auto_now_add=True)
+    # article_image = models.ImageField(upload_to = 'articles/')
+
